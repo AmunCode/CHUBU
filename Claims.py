67668e4c45016b6d765a4f3c665bc530099d88fa
AmunCode <gjose055@fiu.edu>
2020-12-17 17:02:30 -0500

    working on cliams.py.

--- Claims.py
+++ Claims.py
@@ -1,4 +1,7 @@
 import bs4
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import requests
 
 FedEx_url = 'https://www.fedex.com/apps/onlineclaims/?locale=en_US'
@@ -6,5 +9,9 @@ FedEx_url = 'https://www.fedex.com/apps/onlineclaims/?locale=en_US'
 res = requests.get(FedEx_url)
 bs = bs4.BeautifulSoup(res.text, "html.parser")
 
+driver = webdriver.Chrome
 
-print(bs)
+driver.get(FedEx_url)
+
+tracking_number = driver.find_element_by_id("trackinNumber")
+tracking_number.send_keys("01010101010101")
